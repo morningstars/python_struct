f4a6f6d283fbee39396121206fc10a7d011b6a54
alex <806402267@qq.com>
2020-09-29 17:41:09 +0800

    mysql_base_day01 pm 14:40

--- re/regex1.py
+++ re/regex1.py
@@ -11,8 +11,40 @@ pattern = r'(\w+):(\d+)'
 l = re.findall(pattern, s)
 print(l)
 
-
 # compile对象调用
 regex = re.compile(pattern)
 l2 = regex.findall(s)
 print(l2)
+
+# split 分割
+s3 = 'hello world how  arre you L-boby'
+l3 = re.split(r'\W+', s3)
+l32 = re.split(r'[^\w]+', s3)
+print(l32)
+print(l3)
+
+# sub 替换 使用一个字符串替换正则表达式匹配到的内容
+# 返回替换后的字符串
+s4 = '时间：2020/09/29'
+ns = re.sub(r'/', '-', s4, 0)
+
+# subn返回替换后的字符串和替换了几处
+ns2 = re.subn(r'/', '-', s4, 0)
+print(ns)
+print(ns2)
+
+# finditer 返回迭代器
+s5 = '2020年，建国71周年'
+pattern = r'\d+'
+it = re.finditer(pattern, s5)
+for i in it:
+    print(i.group())
+
+# fullmatch 完全匹配
+m = re.fullmatch(r'\w+', 'hello-1973')
+print(m.group())
+
+
+# match 匹配
+m = re.match(r'\w+', 'hello-1973')
+print(m.group())
